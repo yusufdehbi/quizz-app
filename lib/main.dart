b0ea01855ecf61b15edfe49ceac8938c3b258aa6
Youssef Dehbi <youssefdehbi59@gmail.com>
2023-02-25 20:35:40 +0100

    UI Updated

--- lib/main.dart
+++ lib/main.dart
@@ -14,50 +14,88 @@ class quizzApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Quizz'),
         ),
-        body: Column(
-                          // crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-
-            Expanded(
-              child: Center(
-                child: Text("Here where the question will be shown",
-                  style: TextStyle(
-                      color: Colors.white,
-                    fontSize: 20.0
-                  ),
-
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 70.0,
-              width: double.infinity,
-
-              child: ElevatedButton(
-                
-                onPressed: () {},
-                child: Text('True'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 70.0,
-              width: double.infinity,
-              child: ElevatedButton(
-
-                onPressed: () {},
-                child: Text('False'),
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red,
-
-                ),
-              ),
-            ),
-          ],
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
         ),
       ),
+    );
+  }
+}
+
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'This is where the question text will go.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              child: Text(
+                'True',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.green
+              ),
+              onPressed: () {
+                //The user picked true.
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
+              onPressed: () {
+                //The user picked false.
+              },
+            ),
+          ),
+        ),
+        //TODO: Add a Row here as your score keeper
+      ],
     );
   }
 }
